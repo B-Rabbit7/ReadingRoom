2c47e745e373569e195cd2b3043c4ae6277f8e83
Leanne Holmes <lholmes24@my.bcit.ca>
2024-02-14 15:17:44 -0800

    Fixed login code

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,24 +1,27 @@
 import { useNavigate, Link } from "react-router-dom";
 import fetch from "../utils/fetch";
-
-const LoginUser = async () => {
-  const navigate = useNavigate();
-  const data = {
-    email: document.getElementById("email")!.value;
-    email: document.getElementById("password")!.value;
-  }
-  try {
-    await fetch.post("/auth/login", data);
-    console.log("Login successful");
-    navigate("/home");
-  } catch (error) {
-    console.log(error);
-    return error;
-  }
-};
-
+import { useState } from "react";
 
 function Login() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const onLogin = async (event: React.MouseEvent<HTMLElement>) => {
+    event.preventDefault();
+    const data = {
+      email: email,
+      password: password,
+    };
+    try {
+      await fetch.post("/auth/login", data);
+      console.log("Login successful");
+      navigate("/home");
+    } catch (error) {
+      console.log(error);
+      return error;
+    }
+  };
+
   return (
     <div>
       <h3>Login Page</h3>
@@ -31,6 +34,7 @@ function Login() {
               id="email"
               placeholder="Email Address"
               required
+              onChange={(e) => setEmail(e.target.value)}
             ></input>
           </div>
           <div>
@@ -40,9 +44,10 @@ function Login() {
               id="password"
               placeholder="Enter password"
               required
+              onChange={(e) => setPassword(e.target.value)}
             ></input>
           </div>
-          <button onClick={LoginUser}>Login</button>
+          <button onClick={onLogin}>Login</button>
         </form>
         Need to register for an account? <Link to="/">Sign up here</Link>
       </div>
